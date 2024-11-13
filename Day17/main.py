be344c725d009a1525addc49424d7cdeb45b38ae
Simran <simrant009@gmail.com>
2024-11-13 18:29:23 +0545

    quiz game complete

---
+++ Day17/main.py
@@ -0,0 +1,22 @@
+class User:
+    def __init__(self,user_id, username,):
+        self.id  = user_id
+        self.username = username
+        self.follower = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.follower +=1
+        self.following += 1
+
+
+user_1 = User("001", "Simran")
+
+
+user_2 = User("002", "Tamang")
+
+user_1.follow(user_2)
+print(user_1.follower)
+print(user_1.following)
+print(user_2.follower)
+print(user_2.following)
