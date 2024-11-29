b93b2e4a9d46b5f11e9e9290238e4b38a226a173
Simran <simrant009@gmail.com>
2024-11-29 19:09:32 +0545

    pong game complete

---
+++ Day22PongGame/main.py
@@ -0,0 +1,52 @@
+
+from turtle import  Screen
+from paddle import Paddle
+from  ball import Ball
+import time
+from scoreboard import ScoreBoard
+
+screen = Screen()
+
+screen.bgcolor("black")
+screen.setup(width=800,height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+paddle1 = Paddle((350,0))
+paddle2 = Paddle((-350,0))
+
+ball =Ball()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(fun=paddle1.move_up, key="Up")
+screen.onkey(fun=paddle1.move_down, key="Down")
+
+screen.onkey(fun=paddle2.move_up, key="w")
+screen.onkey(fun=paddle2.move_down, key="s")
+game_is_on = True
+
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    # print("in")
+    # detect collision
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # print("out")
+        ball.bounce_y()
+
+
+    #detect collision with right_paddle
+    if ball.distance(paddle1) < 50 and ball.xcor() > 320 or ball.distance(paddle2) <50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+screen.exitonclick()
---
+++ Day22PongGame/paddle.py
@@ -0,0 +1,23 @@
+from turtle import Screen, Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self,position):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.color("white")
+        self.penup()
+        self.setposition(position)
+
+    def move_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def move_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+
+
